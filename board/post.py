7425de853e1fa2f1e6a788ef5e5bfa9fc289b9ed
yongseong88 <crosswindy@naver.com>
2026-06-17 10:15:06 +0900

    post api senario add

--- board/post.py
+++ board/post.py
@@ -43,7 +43,7 @@ class PostsAction(BaseApi):
             return False
 
 
-    def posts_api(self):
+    def specific_post_api(self):
         try:
             posts_url = f"{self.base_url}/posts"
 
@@ -57,9 +57,52 @@ class PostsAction(BaseApi):
             print(f"\npost_lst 길이: {len(post_lst)}")
 
             for index, pl in enumerate(post_lst):
-                single_posts_url = posts_url + f"/{index+1}"
-                single_posts = self.request('get', single_posts_url, headers=headers)
-                print(f"single_info: {single_posts.json()}")
+                specific_posts_url = posts_url + f"/{index+1}"
+                specific_posts = self.request('get', specific_posts_url, headers=headers)
+                specific_post_dict = specific_posts.json()
+                print(f"{index+1}번째 글, id: {specific_post_dict['id']}, 게시글 status_code: {specific_posts.status_code}")
+                # print(f"single_info: {single_posts.json()}")
+
+            # return self.request('get', posts_url, headers=headers)
+
+        except Exception as e:
+            print(f"user_status_api Exception: {e}")
+            return False
+
+    def specific_user_post_api(self):
+        try:
+            id_lst = []
+
+            posts_url = f"{self.base_url}/posts"
+
+            headers = {
+                'accept': 'application/json'
+
+            }
+
+            posts_lists = self.request('get', posts_url, headers=headers)
+            post_lst = posts_lists.json()
+            print(f"\npost_lst 길이: {len(post_lst)}")
+
+            for pl in post_lst:
+                user_id = pl['userId']
+
+                if user_id not in id_lst:
+                    id_lst.append(user_id)
+
+                else:
+                    pass
+
+            for user in id_lst:
+                param_info = {
+                    "userId": user
+                }
+
+                specific_user_post = self.request('get', posts_url, headers=headers, params=param_info)
+                specific_user_post_lst = specific_user_post.json()
+
+                for index, sup in enumerate(specific_user_post_lst):
+                    print(f"userId: {user}의 {index + 1}번째 글, id: {sup['id']}, 게시글 status_code: {specific_user_post.status_code}")
 
             # return self.request('get', posts_url, headers=headers)
 
